70b7369e01dcb2491ff6f1c1241daff6a7d57a77
michael golden <michaelwilliamgolden@gmail.com>
2022-06-20 03:14:36 -0700

    initial

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          test this push<a href="https://nextjs.org">bitch!</a>
         </h1>
 
         <p className={styles.description}>
